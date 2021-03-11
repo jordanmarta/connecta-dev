8ab8bc63ce0661f3e47279216c52ddbc70a17d40
Jordan Marta <jordan.smta@gmail.com>
2021-03-11 15:32:18 -0300

    Criando tela de login 2

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Box,
@@ -9,45 +9,103 @@ import {
   Link,
   FormHelperText,
 } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { makeStyles } from "@material-ui/styles";
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   /* Flex Container */
   root: {
-    display: "flex",
-    flexDirection: "row",
+    // display: "flex",
+    // flexDirection: "row",
     height: "100vh",
   },
-  /* Flex Item container (container por causa da centralização) */
-  left: {
-    background: "blue",
-    flexGrow: 0,
-    flexBasis: "58%",
-
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center", // Alinhamento na vertical
-    alignItems: "center", // Alinhamento na horizontal
+  image: {
+    backgroundImage: "url(/images/background.jpg)",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "none",
+    padding: theme.spacing(1),
+    textAlign: "center",
   },
-  right: {
-    background: "yellow",
-    flexGrow: 0,
-    flexBasis: "42%",
+  avatar: {
+    background: theme.palette.primary.main,
+    marginBottom: theme.spacing(1),
+  },
+  button: {
+    marginTop: theme.spacing(1),
   },
   form: {
-    display: "flex",
-    flexDirection: "column",
-    margin: "62px 32px",
-    alignItems: "center",
+    margin: theme.spacing(0, 4),
   },
+  // estilos antes da aplicação do material ui
+  /* Flex Item container (container por causa da centralização) */
+  //   left: {
+  //     background: "blue",
+  //     flexGrow: 0,
+  //     flexBasis: "58%",
+
+  //     display: "flex",
+  //     flexDirection: "column",
+  //     justifyContent: "center", // Alinhamento na vertical
+  //     alignItems: "center", // Alinhamento na horizontal
+  //   },
+  //   right: {
+  //     background: "yellow",
+  //     flexGrow: 0,
+  //     flexBasis: "42%",
+  //   },
+  //   form: {
+  //     display: "flex",
+  //     flexDirection: "column",
+  //     margin: "62px 32px",
+  //     alignItems: "center",
+  //   },
 }));
+
+function CopyRight() {
+  return (
+    <Typography variant="body2" align="center">
+      {"Copyright © "}
+      <a color="inherit" href="https://www.linkedin.com/in/jordan-santos-marta-2418b2115/">
+        Jordan S. Marta
+      </a>{" "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+function handleSignIn() {
+  axios
+    .get("https://api.github.com/users/jordanmarta")
+    .then((response) => {
+      const user = response.data.login;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 function SignIn() {
   const classes = useStyles();
-
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   return (
-    <div className={classes.root}>
-      <div className={classes.left}>
+    <Grid container className={classes.root}>
+      <Grid
+        item
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        md={7}
+        className={classes.image}
+      >
         <Typography
           style={{
             color: "#FFF",
@@ -68,17 +126,96 @@ function SignIn() {
         >
           Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de software.
         </Typography>
-      </div>
+      </Grid>
+      <Grid item md={5}>
+        <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h4">Acesso</Typography>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={handleSignIn}
+            >
+              Entrar
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link>Esqueceu sua senha?</Link>
+              </Grid>
+              <Grid item>
+                <Link>Não tem uma conta? Registre-se</Link>
+              </Grid>
+            </Grid>
+          </form>
+          <CopyRight />
+        </Box>
+      </Grid>
+    </Grid>
 
-      <div className={classes.right}>
-        <form className={classes.form}>
-          <h4>Acesso</h4>
-          <input type="text"></input>
-          <input type="text"></input>
-        </form>
-        Form
-      </div>
-    </div>
+    // <div className={classes.root}>
+    //   <div className={classes.left}>
+    //     <Typography
+    //       style={{
+    //         color: "#FFF",
+    //         fontSize: 35,
+    //         lineHeight: "45px",
+    //       }}
+    //     >
+    //       <strong>Simplificando a forma de conectar desenvolvedores de software!</strong>
+    //     </Typography>
+    //     <Typography
+    //       variant="body2"
+    //       style={{
+    //         color: "rgb(255, 255, 255, 0.7)",
+    //         marginTop: 30,
+    //         fontSize: 15,
+    //         lineHeight: "30px",
+    //       }}
+    //     >
+    //       Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de software.
+    //     </Typography>
+    //   </div>
+
+    //   <div className={classes.right}>
+    //     <form className={classes.form}>
+    //       <h4>Acesso</h4>
+    //       <input type="text"></input>
+    //       <input type="text"></input>
+    //     </form>
+    //     Form
+    //   </div>
+    // </div>
   );
 }
 export default SignIn;
