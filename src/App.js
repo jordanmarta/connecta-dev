945547794c8977a19d22489ee456425637d4c8bc
Jordan Marta <jordan.smta@gmail.com>
2021-03-11 10:18:19 -0300

    Adicionando e configurando Rotas (react-router 6)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Home from "./pages/Home";
 import { ThemeProvider } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 import theme from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />j
+          <Route path="*" element={<h1>Not Found 404!</h1>} />j
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
