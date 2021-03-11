85ff88d74604a009488085be32cc5461cd145263
Jordan Marta <jordan.smta@gmail.com>
2021-03-11 15:33:03 -0300

    Removendo código antigo - Tela de Login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -16,10 +16,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
-  /* Flex Container */
   root: {
-    // display: "flex",
-    // flexDirection: "row",
     height: "100vh",
   },
   image: {
@@ -40,29 +37,6 @@ const useStyles = makeStyles((theme) => ({
   form: {
     margin: theme.spacing(0, 4),
   },
-  // estilos antes da aplicação do material ui
-  /* Flex Item container (container por causa da centralização) */
-  //   left: {
-  //     background: "blue",
-  //     flexGrow: 0,
-  //     flexBasis: "58%",
-
-  //     display: "flex",
-  //     flexDirection: "column",
-  //     justifyContent: "center", // Alinhamento na vertical
-  //     alignItems: "center", // Alinhamento na horizontal
-  //   },
-  //   right: {
-  //     background: "yellow",
-  //     flexGrow: 0,
-  //     flexBasis: "42%",
-  //   },
-  //   form: {
-  //     display: "flex",
-  //     flexDirection: "column",
-  //     margin: "62px 32px",
-  //     alignItems: "center",
-  //   },
 }));
 
 function CopyRight() {
@@ -182,40 +156,6 @@ function SignIn() {
         </Box>
       </Grid>
     </Grid>
-
-    // <div className={classes.root}>
-    //   <div className={classes.left}>
-    //     <Typography
-    //       style={{
-    //         color: "#FFF",
-    //         fontSize: 35,
-    //         lineHeight: "45px",
-    //       }}
-    //     >
-    //       <strong>Simplificando a forma de conectar desenvolvedores de software!</strong>
-    //     </Typography>
-    //     <Typography
-    //       variant="body2"
-    //       style={{
-    //         color: "rgb(255, 255, 255, 0.7)",
-    //         marginTop: 30,
-    //         fontSize: 15,
-    //         lineHeight: "30px",
-    //       }}
-    //     >
-    //       Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de software.
-    //     </Typography>
-    //   </div>
-
-    //   <div className={classes.right}>
-    //     <form className={classes.form}>
-    //       <h4>Acesso</h4>
-    //       <input type="text"></input>
-    //       <input type="text"></input>
-    //     </form>
-    //     Form
-    //   </div>
-    // </div>
   );
 }
 export default SignIn;
